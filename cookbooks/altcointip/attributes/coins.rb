5001fb51bd57f9501b04863bca01e2fd4a91a2a4
vindimy <vindimy@gmail.com>
2013-09-08 23:53:33 +0000

    Adding Namecoin and Primecoin

--- cookbooks/altcointip/attributes/coins.rb
+++ cookbooks/altcointip/attributes/coins.rb
@@ -1,5 +1,5 @@
-node[:altcointip][:coins] = [
-  {
+node[:altcointip][:coins] = {
+  "bitcoin" => {
     :enabled => true,
     :name => "bitcoin",
     :version => "0.8.4",
@@ -11,7 +11,7 @@ node[:altcointip][:coins] = [
     :rpcpassword => "bitcoin123",
     :rpcport => "8332"
   },
-  {
+  "litecoin" => {
     :enabled => true,
     :name => "litecoin",
     :version => "0.8.4.1",
@@ -23,7 +23,7 @@ node[:altcointip][:coins] = [
     :rpcpassword => "litecoin123",
     :rpcport => "9332"
   },
-  {
+  "ppcoin" => {
     :enabled => true,
     :name => "ppcoin",
     :version => "0.3.0",
@@ -34,6 +34,30 @@ node[:altcointip][:coins] = [
     :daemon_path => "bin/64/ppcoind",
     :rpcpassword => "ppcoin123",
     :rpcport => "9902"
-
+  },
+  "namecoin" => {
+    :enabled => true,
+    :name => "namecoin",
+    :version => "0.3.50",
+    :file_name => "namecoin_linux64.tgz",
+    :file_source_url => "http://dot-bit.org/files/namecoin_linux64.tgz",
+    :file_sha256sum => "2ecd4a17d1c301da3dc60e60af189690a8e7abb1b83fdabbfb4c2951e25bc2ec",
+    :file_extract_cmd => "tar -zxf",
+    :daemon_path => "namecoind",
+    :rpcpassword => "namecoin123",
+    :rpcport => "8336"
+  },
+  "primecoin" => {
+    :enabled => true,
+    :name => "primecoin",
+    :version => "0.1.1",
+    :file_name => "primecoin-0.1.1-linux.tar.gz",
+    :file_source_url => "http://sourceforge.net/projects/primecoin/files/0.1.1/primecoin-0.1.1-linux.tar.gz/download",
+    :file_sha256sum => "e9c2a34ddf6806924139ac5a1294f0e24f7480604773faaf79df58dc394f1594",
+    :file_extract_cmd => "tar --strip-components=1 -zxf",
+    :daemon_path => "bin/64/primecoind",
+    :rpcpassword => "primecoin123",
+    :rpcport => "8772"
+  }
 ]
 
